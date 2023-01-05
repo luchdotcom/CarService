b9bc80fd21ffe0d35982bc46e231614c84f63dd3
luchdotcom <lachezarivanov05|@gmail.com>
2023-01-06 00:31:51 +0200

    modified CarServiceImplementations CarService

--- CarService/src/main/java/com/informatics/CarService/services/implamentations/CarServiceImplementations.java
+++ CarService/src/main/java/com/informatics/CarService/services/implamentations/CarServiceImplementations.java
@@ -5,7 +5,7 @@ import com.informatics.CarService.data.repository.CarRepository;
 import com.informatics.CarService.services.interfaces.CarService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
+import org.springframework.data.domain.PageRequest;
 import java.util.List;
 
 @Service
@@ -13,15 +13,41 @@ import java.util.List;
 public class CarServiceImplementations implements CarService {
     private final CarRepository carRepository;
 
-    public CarServiceImplementations(CarRepository carRepository) {
-        this.carRepository = carRepository;
-    }
+//    public CarServiceImplementations(CarRepository carRepository) {
+//        this.carRepository = carRepository;
+//    }
 
     @Override
     public List<Car> getCars() {
         return carRepository.findAll();
     }
-    public Car create(Car car){
+
+    public Car getCarByRegId(String regId) {
+        return null;
+    }
+
+    @Override
+    public Car create(Car car) {
         return carRepository.save(car);
+    }
+
+    public Car getCarById(long id) {
+        return carRepository.findById(id);
+//                .orElseThrow(() -> new IllegalAccessException("invalid Car id: " + id));
+    }
+
+    @Override
+    public Car updateCar(long id, Car upCar) {
+//        upCar.setId(id);
+//        return carRepository.save(upCar);
+        return null;
+    }
+
+    //    public Car updateCarById(long id){
+//        carRepository.updateById(id);
+//    }
+    @Override
+    public void deleteCar(long id) {
+        carRepository.deleteById(id);
     }
 }
--- CarService/src/main/java/com/informatics/CarService/services/interfaces/CarService.java
+++ CarService/src/main/java/com/informatics/CarService/services/interfaces/CarService.java
@@ -6,5 +6,12 @@ import java.util.List;
 
 public interface CarService {
     List<Car> getCars();
+    Car getCarById(long id);
+
+    Car getCarByRegId(String regId);//todo
+
     Car create(Car car);
+    Car updateCar(long id,Car car);//todo
+    void deleteCar(long id);
+
 }
