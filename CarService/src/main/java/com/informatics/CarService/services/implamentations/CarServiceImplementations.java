adb6cda09bae8cfed109f2bc008726e9b9f29cea
luchdotcom <lachezarivanov05|@gmail.com>
2023-01-27 09:33:11 +0200

    modifi carService

--- CarService/src/main/java/com/informatics/CarService/services/implamentations/CarServiceImplementations.java
+++ CarService/src/main/java/com/informatics/CarService/services/implamentations/CarServiceImplementations.java
@@ -2,52 +2,61 @@ package com.informatics.CarService.services.implamentations;
 
 import com.informatics.CarService.data.entitys.Car;
 import com.informatics.CarService.data.repository.CarRepository;
+import com.informatics.CarService.dto.CarDTO;
+import com.informatics.CarService.dto.CreateCarDTO;
 import com.informatics.CarService.services.interfaces.CarService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-import org.springframework.data.domain.PageRequest;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class CarServiceImplementations implements CarService {
     private final CarRepository carRepository;
 
-//    public CarServiceImplementations(CarRepository carRepository) {
-//        this.carRepository = carRepository;
-//    }
 
+    private  final ModelMapper modelMapper;
     @Override
-    public List<Car> getCars() {
-        return carRepository.findAll();
+    public List<CarDTO> getCars() {
+        return carRepository.findAll().stream().map(this::convertToCarDTO).collect(Collectors.toList());
     }
 
     public Car getCarByRegId(String regId) {
-        return null;
+        return carRepository.findByRegID(regId);
     }
 
     @Override
-    public Car create(Car car) {
-        return carRepository.save(car);
+    public Car create(CreateCarDTO createCarDTO) {
+        return carRepository.save(modelMapper.map(createCarDTO,Car.class));
     }
 
     public Car getCarById(long id) {
-        return carRepository.findById(id);
-//                .orElseThrow(() -> new IllegalAccessException("invalid Car id: " + id));
+        return carRepository.findById(id)
+                .orElseThrow(() -> new CarNotFoundException("invalid Car id: " + id));
     }
 
+
+//    @Override
+//    public Car getCar(long id) {
+//        return carRepository.findById(id);
+////                .orElseThrow(() ->new IllegalAccessException("invalid Car id =" +id));
+//    }
     @Override
     public Car updateCar(long id, Car upCar) {
-//        upCar.setId(id);
-//        return carRepository.save(upCar);
-        return null;
+        upCar.setId(id);
+        return carRepository.save(upCar);
+//        return null;
     }
 
-    //    public Car updateCarById(long id){
-//        carRepository.updateById(id);
-//    }
+
     @Override
     public void deleteCar(long id) {
         carRepository.deleteById(id);
+    }
+    private CarDTO convertToCarDTO(Car car){
+        return modelMapper.map(car,CarDTO.class);
     }
 }
--- CarService/src/main/java/com/informatics/CarService/services/interfaces/CarService.java
+++ CarService/src/main/java/com/informatics/CarService/services/interfaces/CarService.java
@@ -1,16 +1,18 @@
 package com.informatics.CarService.services.interfaces;
 
 import com.informatics.CarService.data.entitys.Car;
+import com.informatics.CarService.dto.CarDTO;
+import com.informatics.CarService.dto.CreateCarDTO;
 
 import java.util.List;
 
 public interface CarService {
-    List<Car> getCars();
+    List<CarDTO> getCars();
     Car getCarById(long id);
 
-    Car getCarByRegId(String regId);//todo
+    Car getCarByRegId(String regId);
 
-    Car create(Car car);
+    Car create(CreateCarDTO createCarDTO);
     Car updateCar(long id,Car car);//todo
     void deleteCar(long id);
 
