10bacc501a75dab0c16957e16cef876e01f963a1
luchdotcom <lachezarivanov05|@gmail.com>
2023-01-27 09:31:54 +0200

    modifi car repository

--- CarService/src/main/java/com/informatics/CarService/data/repository/CarRepository.java
+++ CarService/src/main/java/com/informatics/CarService/data/repository/CarRepository.java
@@ -1,6 +1,7 @@
 package com.informatics.CarService.data.repository;
 
 import com.informatics.CarService.data.entitys.Car;
+import com.informatics.CarService.dto.CarDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,9 +10,6 @@ public interface CarRepository extends JpaRepository<Car,Long> {
 
     List<Car> findAllByCarMark(String mark);
     List<Car> countCarByCarMark(String CarMark);
-    Car findByRegID(String regId);//todo
-    Car findById(long id);//todo
-
-    List <Car> findAll();
+    Car findByRegID(String regID);//todo
 
 }
